4d3ace1f7c4db64a9234b3f805378a77ba82d4eb
Angelo Martinez <amart670@fiu.edu>
2020-07-18 21:17:24 -0400

    added new logos and redirects

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 console.log('works');
-
-var bgVid = document.getElementById('bgVideo');
-
-if (bgVid) {
-  vid.playbackRate = 0.4;
-}
---
+++ login.js
@@ -0,0 +1,13 @@
+var bgVid = document.getElementById('bgVideo');
+var logInBtn = document.querySelector('.js-login');
+var registerBtn = document.querySelector('.js-register');
+
+bgVid.playbackRate = 0.4;
+
+logInBtn.addEventListener('click', () => {
+  document.querySelector('.js-login-wrapper').classList.add('is-logging-in');
+});
+
+registerBtn.addEventListener('click', () => {
+  window.location.href = '/register';
+});
