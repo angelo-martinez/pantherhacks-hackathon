8b0eeabfa23bfd947bc2eebb5cf92b9937de3b4a
Angelo Martinez <amart670@fiu.edu>
2020-07-19 15:35:43 -0400

    added persistent state

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,23 @@ import { Link } from "gatsby";
 import logo from "../images/logo.png";
 import React from "react";
 
-const Header = props => (
-  <nav className="nav-bar">
-    <Link to="/">
-      <img src={logo} alt="logo" className="logo" />
-    </Link>
-    {props.button && (
-      <Link to="/login" className="btn btn-a">
-        <span>Log in</span>
+const Header = props => {
+  let loggedInAcc = {};
+  if (typeof window !== "undefined") {
+    loggedInAcc = window.loggedInAcc;
+  }
+  return (
+    <nav className="nav-bar">
+      <Link to="/">
+        <img src={logo} alt="logo" className="logo" />
       </Link>
-    )}
-  </nav>
-);
+      {props.button && (
+        <Link to={loggedInAcc ? "/dashboard" : "/login"} className="btn btn-a">
+          <span>{loggedInAcc ? "Dashboard" : "Log in"}</span>
+        </Link>
+      )}
+    </nav>
+  );
+};
 
 export default Header;
